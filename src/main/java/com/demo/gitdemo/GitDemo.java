e6caefc9b11dfaaa87a1147f322c9fc8e7b99eda
lmh <2447613044@qq.com>
2022-10-16 21:50:09 +0800

    lmh commit git-demo 4

--- src/main/java/com/demo/gitdemo/GitDemo.java
+++ src/main/java/com/demo/gitdemo/GitDemo.java
@@ -5,5 +5,6 @@ public class GitDemo {
         System.out.println("hello git!");
         System.out.println("hello git!");
         System.out.println("hello git!");
+        System.out.println("hello git! master");
     }
 }
